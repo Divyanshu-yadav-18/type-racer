ef19ddc85452643cafbae896bbcc774b8ce6dc1c
Divyanshu-yadav-18 <rishabhyadav14065@gmail.com>
2025-03-04 23:55:30 +0530

    mongodb connect

--- server/index.js
+++ server/index.js
@@ -7,6 +7,19 @@ const port = 3000;
 
 var server = http.createServer(app);
 
+app.use(express.json());
+
+const dB = `mongodb+srv://ryuk:${mongo_password}@cluster0.eor9i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose
+  .connect(dB)
+  .then(() => {
+    console.log("connection Successful");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
 server.listen(port, "0.0.0.0", () => {
   console.log("server started");
 });
