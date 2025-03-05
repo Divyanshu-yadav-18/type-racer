cfdd561554fce07026cc68d43012a6d75eba25fb
Divyanshu-yadav-18 <rishabhyadav14065@gmail.com>
2025-03-05 21:23:32 +0530

    connect socket.io

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const { Socket } = require("dgram");
 const express = require("express");
 const http = require("http");
 const mongoose = require("mongoose");
@@ -8,9 +9,15 @@ const port = 3000;
 
 var server = http.createServer(app);
 
+var io = require("socket.io")(server);
+
 app.use(express.json());
 
 const dB = process.env.MONGO_URI;
+
+io.on("connection", (socket) => {
+  console.log(socket.io);
+});
 
 mongoose
   .connect(dB)
